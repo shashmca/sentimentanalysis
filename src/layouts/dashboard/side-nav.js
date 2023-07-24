93af8798dc24cae6cf7697018ed09fe84f31ec1f
Shrivastava <shashank.shrivastava@adidas.com>
2023-07-24 21:15:27 +0530

    dashboard added

---
+++ src/layouts/dashboard/side-nav.js
@@ -0,0 +1,67 @@
+import NextLink from 'next/link';
+import { usePathname } from 'next/navigation';
+import PropTypes from 'prop-types';
+import ArrowTopRightOnSquareIcon from '@heroicons/react/24/solid/ArrowTopRightOnSquareIcon';
+import ChevronUpDownIcon from '@heroicons/react/24/solid/ChevronUpDownIcon';
+import {
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  Stack,
+  SvgIcon,
+  Typography,
+  useMediaQuery
+} from '@mui/material';
+import { Logo } from 'src/components/logo';
+import { Scrollbar } from 'src/components/scrollbar';
+import { items } from './config';
+
+
+export const SideNav = (props) => {
+  const { open, onClose } = props;
+  const pathname = usePathname();
+  const lgUp = useMediaQuery((theme) => theme.breakpoints.up('lg'));
+
+  if (lgUp) {
+    return (
+      <Drawer
+        anchor="left"
+        open
+        PaperProps={{
+          sx: {
+            backgroundColor: 'neutral.800',
+            color: 'common.white',
+            width: 0
+          }
+        }}
+        variant="permanent"
+      >
+      </Drawer>
+    );
+  }
+
+  return (
+    <Drawer
+      anchor="left"
+      onClose={onClose}
+      open={open}
+      PaperProps={{
+        sx: {
+          backgroundColor: 'neutral.800',
+          color: 'common.white',
+          width: 280
+        }
+      }}
+      sx={{ zIndex: (theme) => theme.zIndex.appBar + 100 }}
+      variant="temporary"
+    >
+      {content}
+    </Drawer>
+  );
+};
+
+SideNav.propTypes = {
+  onClose: PropTypes.func,
+  open: PropTypes.bool
+};
---
+++ src/pages/index.js
@@ -0,0 +1,99 @@
+import Head from 'next/head';
+import { Box, Container, Unstable_Grid2 as Grid } from '@mui/material';
+import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
+import { OverviewBudget } from 'src/sections/overview/overview-budget';
+import { OverviewSales } from 'src/sections/overview/overview-sales';
+import { OverviewTotalCustomers } from 'src/sections/overview/overview-total-customers';
+import { OverviewTraffic } from 'src/sections/overview/overview-traffic';
+
+const now = new Date();
+
+const Page = () => (
+  <>
+    <Head>
+      <title>
+        Overview | Devias Kit
+      </title>
+    </Head>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth="xl">
+        <Grid
+          container
+          spacing={3}
+        >
+          <Grid
+            xs={12}
+            sm={6}
+            lg={3}
+          >
+            <OverviewBudget
+              difference={12}
+              positive
+              sx={{ height: '100%' }}
+              value="50k"
+            />
+          </Grid>
+          <Grid
+            xs={12}
+            sm={6}
+            lg={3}
+          >
+            <OverviewTotalCustomers
+              difference={16}
+              positive={false}
+              sx={{ height: '100%' }}
+              value="12.6k"
+            />
+          </Grid>
+          <Grid
+            xs={12}
+            lg={8}
+          >
+            <OverviewSales
+              chartSeries={[
+                {
+                  name: 'Positive',
+                  data: [18, 16, 5, 8, 3, 14, 14, 16, 17]
+                },
+                {
+                  name: 'Negative',
+                  data: [12, 11, 4, 6, 2, 9, 9, 10, 11]
+                },
+                {
+                  name: 'Neutral',
+                  data: [8, 7, 1, 3, 0, 5, 4, 3, 1]
+                }
+              ]}
+              sx={{ height: '100%' }}
+            />
+          </Grid>
+          <Grid
+            xs={12}
+            md={6}
+            lg={4}
+          >
+            <OverviewTraffic
+              chartSeries={[63, 15, 22]}
+              labels={['Positive', 'Negative', 'Neutral']}
+              sx={{ height: '100%' }}
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  </>
+);
+
+Page.getLayout = (page) => (
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
+);
+
+export default Page;
