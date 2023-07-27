10425e47fc21bbd0c4f1fdbeace3ddb0640b2f4c
Toppo, Edwin <Edwin.Toppo@adidas.com>
2023-07-27 21:02:09 +0530

    Updating chatbot.js

--- react/src/components/chatbot.js
+++ react/src/components/chatbot.js
@@ -1,34 +1,61 @@
 import { Configuration, OpenAIApi } from 'openai';
-import { useState } from 'react';
-import { Button, Input } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Button, Card, CardContent, CardHeader, Input } from '@mui/material';
+import PropTypes from 'prop-types';
 
-export const ChatBot = () => {
+export const ChatBot = (props) => {
     const [reply, updateReply] = useState('');
     const config = new Configuration({
         organization: 'org-aRAe4TSpaU6a8nky8YGtInFC',
-        apiKey: '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'
+        apiKey: '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'
     });
     delete config.baseOptions.headers['User-Agent'];
     const openai = new OpenAIApi(config);
 
-    const [input, setInput] = useState('Bot user')
+    const input = 'provide me 3 options on how Adidas can boost customer experience based on sentiment analysis report';
 
-    const handleClick = () => {
+    useEffect(() => {
+        console.log('rendering');
+        const storedResponse = localStorage.getItem('genAIResponse')
+        if(!storedResponse){
         const getAIReply = async () => {
+            console.log('api claling');
             const response = await openai.createChatCompletion({
                 model: "gpt-3.5-turbo",
                 messages: [{"role": "system", "content": "You are a helpful assistant"}, {role: "user", content: input}]
             });
+            console.log('api respose getting');
             updateReply(response.data.choices[0].message.content);
+            localStorage.setItem('genAIResponse', response.data.choices[0].message.content)
         }
-        getAIReply();
-      }
+
+        getAIReply();}
+        updateReply(storedResponse);
+    }, [])
+    
+    useEffect(()=>{
+
+        console.log('reply updated');
+    },[reply])
+    // const handleClick = () => {
+        
+    //   }
 
     return (
-        <>
-            <Button className='shub' onClick={()=> handleClick()}>abc</Button>
-            {<Input type='text' onChange={(e) => setInput(e.target.value)}/>}
-            {reply && <div>{reply}</div>}
-        </>
+        // <>
+        //     <Button className="fetch-ai-suggestions" onClick={()=> handleClick()}>abc</Button>
+        //     {<Input type='text' onChange={(e) => setInput(e.target.value)}/>}
+        //     {reply && <div>{reply}</div>}
+        // </>
+        <Card sx={props.sx}>
+            <CardHeader title="AI Suggestions" />
+            <CardContent>
+                {reply!='' && reply}
+            </CardContent>
+        </Card>
     );
+}
+
+ChatBot.protoTypes = {
+    sx: PropTypes.object
 }
--- react/src/pages/_app.js
+++ react/src/pages/_app.js
@@ -27,7 +27,7 @@ const App = (props) => {
     <CacheProvider value={emotionCache}>
       <Head>
         <title>
-          Devias Kit
+          Adi Senti
         </title>
         <meta
           name="viewport"
